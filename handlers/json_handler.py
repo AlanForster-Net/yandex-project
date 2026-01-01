42cca27b0b706833ed010caea820b10db2f1d9e4
knowyx <knowyx@gmail.com>
2026-01-01 23:53:54 +0300

    screen choose now writes to a file

--- handlers/json_handler.py
+++ handlers/json_handler.py
@@ -5,3 +5,13 @@ def reader(path):
     with open(path, mode='r', encoding='utf-8') as f:
         data = json.load(f)
     return data
+
+
+def writer(path, screen, num):
+    data = dict()
+    data["screenNum"] = num
+    data["screenName"] = screen.get_monitor_name()
+    data["screenWidth"] = screen.width
+    data["screenHeight"] = screen.height
+    with open(path, mode = 'w', encoding='utf-8') as f:
+        json.dump(data, f, indent=4)
--- menu/screen_dialog.py
+++ menu/screen_dialog.py
@@ -1,22 +1,29 @@
 import arcade
 from arcade import get_screens
-from arcade.gui import UIManager, UIFlatButton, UITextureButton, UILabel
+from arcade.gui import UIManager, UIDropdown, UIFlatButton
 from arcade.gui.widgets.layout import UIAnchorLayout, UIBoxLayout
+from handlers.json_handler import writer
 
 
+JSONPATH = 'data/cfg.json'
 TITLE = "Выберите экран для запуска"
 SCREEN_HEIGHT = 200
 SCREEN_WIDTH = 400
 
 
-class Dialog(arcade.Window):
-    def get_sc(self):
-        screens = arcade.get_screens()
-        print(screens)
+def get_sc():
+    ans = []
+    for x in enumerate(arcade.get_screens()):
+        ans.append(str(x[0] + 1) + ". " + x[1].get_monitor_name() + ' ' + str(x[1].width) + 'x' + str(x[1].height))
+    return ans
 
+
+class Dialog(arcade.Window):
+    CHOSEN = None
 
     def __init__(self):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, title=TITLE, fullscreen=False)
+        self.flat_button = None
         arcade.set_background_color(arcade.color.WHITE)
         self.manager = UIManager()
         self.manager.enable()
@@ -25,11 +32,20 @@ class Dialog(arcade.Window):
         self.setup_widgets()
         self.anchor_layout.add(self.box_layout)
         self.manager.add(self.anchor_layout)
-        self.get_sc()
 
-
-
+    def update_chosen(self, value):
+        self.CHOSEN = int(value.new_value.split('.')[0]) - 1
+        self.flat_button.on_click = lambda event: writer(JSONPATH, arcade.get_screens()[self.CHOSEN], self.CHOSEN)
 
     def setup_widgets(self):
-        pass
+        screens = get_sc()
+        dropdown = UIDropdown(options=screens, width=200, color=arcade.color.BLACK)
+        dropdown.on_change = lambda value: self.update_chosen(value)
+        self.box_layout.add(dropdown)
+        self.flat_button = UIFlatButton(text="Выбрать экран", width=200, height=50, color=arcade.color.BLACK)
+        self.box_layout.add(self.flat_button)
+
+    def on_draw(self):
+        self.clear()
+        self.manager.draw()
 
