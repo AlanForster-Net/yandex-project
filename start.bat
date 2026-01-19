6f272fe510f08c7e6eec40effe8470d60c3c7a35
knowyx <knowyx@gmail.com>
2026-01-19 17:20:49 +0300

    added file for win and readme

---
+++ start.bat
@@ -0,0 +1,8 @@
+@echo off
+echo Running a game "Run from antivirus!"
+
+powershell -ExecutionPolicy Bypass -Command "python3 main.py type='dialog'; if ($LASTEXITCODE -ne 0) {python main.py type='dialog'}"
+
+powershell -ExecutionPolicy Bypass -Command "python3 main.py type='run'; if ($LASTEXITCODE -ne 0) {python main.py type='run'}"
+
+pause
