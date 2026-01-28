2e8eb90ceca287fd2445fbe77003663dabe2678e
knowyx <knowyx@gmail.com>
2026-01-28 12:39:57 +0300

    release 1.5.2 added venv for windows

--- start.bat
+++ start.bat
@@ -1,6 +1,32 @@
 @echo off
+setlocal
+
+set DIRECTORY=.venv
+
+if exist "%DIRECTORY%" (
+    echo Found a venv
+) else (
+    echo Can't find a venv. Creating
+    python -m venv .venv
+    if errorlevel 1 (
+        python3 -m venv .venv
+    )
+)
+call .venv\Scripts\activate.bat
+pip show arcade >nul 2>&1
+if errorlevel 1 (
+    pip install arcade[full]
+)
+echo Venv contains a valid arcade
 echo Running a game "Run from antivirus!"
+python main.py type=dialog
+if errorlevel 1 (
+    python3 main.py type=dialog
+)
+python main.py type=run
+if errorlevel 1 (
+    python3 main.py type=run
+)
+deactivate
 
-powershell -ExecutionPolicy Bypass -Command "python3 main.py type='dialog'; if ($LASTEXITCODE -ne 0) {python main.py type='dialog'}"
-
-powershell -ExecutionPolicy Bypass -Command "python3 main.py type='run'; if ($LASTEXITCODE -ne 0) {python main.py type='run'}"
+endlocal
